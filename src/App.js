d1ebd982d348eb2ba48f2c8430e75fd5427b06f3
肖克松 <1141778924@qq.com>
2020-08-05 15:36:10 +0800

    添加您好

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Good moming, Welcome to React, 肖克松</h1>
+          <h1 className="App-title">
+            Good moming, Welcome to React, 肖克松您好
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
