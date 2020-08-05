9a4b74a92e4d1964f55b4ce3c04d4664cafe0079
肖克松 <1141778924@qq.com>
2020-08-05 20:24:08 +0800

    中文陷阱

--- deploy(simple-node-js-react-npm-app).bat
+++ deploy(simple-node-js-react-npm-app).bat
@@ -1,11 +1,7 @@
-rem 创建备份项目文件夹
 set ymd=%date:~0,4%-%date:~5,2%-%date:~8,2%
 set hms=(%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%)
 set dt=%ymd%%hms%
 mkdir bak\%dt%
-rem 备份项目文件
 copy simple-node-js-react-npm-app bak\%dt%
-rem 删除项目文件夹
 del /s /q simple-node-js-react-npm-app
-rem 覆盖新的项目文件
 copy simple-node-js-react-npm-app@tmp simple-node-js-react-npm-app
