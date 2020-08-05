47f49fad6e7875e00d313fcc8b0ba1ef06d58636
肖克松 <1141778924@qq.com>
2020-08-05 21:13:24 +0800

    再发一版

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,9 @@
-set ymd=%date:~0,4%-%date:~5,2%-%date:~8,2%
-set hms=(%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%)
-set dt=%ymd%%hms%
-mkdir bak\%dt%
-copy simple-node-js-react-npm-app bak\%dt%
-del /s /q simple-node-js-react-npm-app
-xcopy simple-node-js-react-npm-app@tmp\build simple-node-js-react-npm-app /E /Y
+SET ymd=%date:~0,4%-%date:~5,2%-%date:~8,2%
+SET hms=(%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%)
+SET dt=%ymd%%hms%
+MKDIR bak\%dt%
+XCOPY simple-node-js-react-npm-app bak\%dt% /E /Y
+RD /S /Q simple-node-js-react-npm-app
+MKDIR simple-node-js-react-npm-app
+XCOPY simple-node-js-react-npm-app@tmp\build simple-node-js-react-npm-app /E /Y
+RD /S /Q simple-node-js-react-npm-app@tmp\build
